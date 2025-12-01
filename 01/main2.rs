848493b13ddc624ea1cd39852f4327d44aadbade
pawel <paweljarosz002@gmail.com>
2025-12-01 22:52:29 +0100

    update solution

--- 01/main2.rs
+++ 01/main2.rs
@@ -2,9 +2,13 @@ fn main() {
     print!("{}", std::fs::read_to_string("01/input.txt").unwrap().lines().fold((50, 0), |(p, c), l| {
         let v: i32 = l[1..].parse().unwrap();
         let d = l.as_bytes()[0] == b'R';
-        let raw_dest = if d { p + v } else { p - v };
-        let laps = (raw_dest.div_euclid(100) - p.div_euclid(100)).abs();
-        let p_n = raw_dest.rem_euclid(100);
-        (p_n, c + laps + (p_n == 0) as i32)
+        
+        let dest = if d { p + v } else { p - v };
+        let laps = if d {
+            dest.div_euclid(100) - p.div_euclid(100)
+        } else {
+            (p - 1).div_euclid(100) - (dest - 1).div_euclid(100)
+        };
+        (dest.rem_euclid(100), c + laps)
     }).1);
 }
