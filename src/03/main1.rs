e18e1a026b07d987f5dce64ae92d3f969345e059
pawel <paweljarosz002@gmail.com>
2025-12-03 08:34:06 +0100

    add cargo project

---
+++ src/03/main1.rs
@@ -0,0 +1,12 @@
+fn main() {
+    print!(
+        "{}",
+        std::fs::read_to_string("01/input.txt")
+        .unwrap()
+        .lines()
+        .fold(0, |c, l| {
+            for i in 1..len()
+
+        })
+    );
+}
